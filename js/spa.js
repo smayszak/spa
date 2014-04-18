3e6065e8529b11b5c3ef5fe0de4a32b960884e77
Steve Mayszak <stevemayszak@gmail.com>
2014-04-17 22:52:22 -0700

    page 72

---
+++ js/spa.js
@@ -0,0 +1,9 @@
+var spa = (function () {
+  var initModule = function ( $container ) {
+    $container.html(
+      '<h1 style="display:inline-block; margin:25px;">'
+        + 'hello world!'
+      + '</h1>'
+); };
+  return { initModule: initModule };
+}());
